bc374d6f266755cb85cdabec28627b3f511cb807
YanShlain <yan.shlain@gmail.com>
2026-07-09 15:24:07 +0300

    Initial commit: Spring Boot 3.x scaffold
    Maven project with Spring Web and Spring Data JPA starters, Java 21.

---
+++ src/main/java/com/yanshlain/minidem/MinidemApplication.java
@@ -0,0 +1,13 @@
+package com.yanshlain.minidem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidemApplication.class, args);
+    }
+
+}
